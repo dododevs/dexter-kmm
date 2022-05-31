efb11b6d8b514af9d91ea93eb8aa646d01ed7811
Andrea Esposito <aespositox@gmail.com>
2022-05-31 08:15:31 +0200

    Setup kmm project

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Dexter"
+include(":androidApp")
+include(":shared")
